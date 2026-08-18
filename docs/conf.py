a9a5aad959f397fa9b6f88c67cc52c632223ad81
fubootv <ritusahani1920@outlook.com>
2026-08-18 10:48:16 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+project = 'fubo tv-login account'
+author = 'fubo tv-login account'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
